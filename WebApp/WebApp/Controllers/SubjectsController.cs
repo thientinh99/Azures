0cd1596ad30c3216ca33103c8eb41bc23424ee40
Toan Tran <toanquoctran190901@gmail.com>
2021-10-04 09:00:44 +0700

    Ma Sứ

---
+++ WebApp/WebApp/Controllers/SubjectsController.cs
@@ -0,0 +1,286 @@
+﻿using ClosedXML.Excel;
+using LinqToExcel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class SubjectsController : Controller
+    {
+        private Database1Entities db = new Database1Entities();
+
+        // GET: Subjects
+        public ActionResult Index()
+        {
+            var subjects = db.Subjects.Include(s => s.Curriculum);
+            return View(subjects.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult ImportExcel(HttpPostedFileBase postedFile)
+        {
+            if (postedFile != null)
+            {
+                try
+                {
+                    string fileExtension = Path.GetExtension(postedFile.FileName);
+                    ViewBag.Message = "fileExtension" + fileExtension;
+                    //Validate uploaded file and return error.
+                    if (fileExtension != ".xls" && fileExtension != ".xlsx")
+                    {
+                        ViewBag.Message = "Please select the excel file with .xls or .xlsx extension";
+                        return View();
+                    }
+
+                    string folderPath = Server.MapPath("~/UploadedFiles/");
+                    //Check Directory exists else create one
+                    if (!Directory.Exists(folderPath))
+                    {
+                        Directory.CreateDirectory(folderPath);
+                    }
+
+                    //Save file to folder
+                    var filePath = folderPath + Path.GetFileName(postedFile.FileName);
+                    postedFile.SaveAs(filePath);
+
+
+                    //Get file extension
+
+                    string excelConString = "";
+
+                    //Get connection string using extension 
+                    switch (fileExtension)
+                    {
+                        //If uploaded file is Excel 1997-2007.
+                        case ".xls":
+                            excelConString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='Excel 8.0;HDR=YES'";
+                            break;
+                        //If uploaded file is Excel 2007 and above
+                        case ".xlsx":
+                            excelConString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR=YES'";
+                            break;
+                    }
+                    //Read data from first sheet of excel into datatable
+                    DataTable dt = new DataTable();
+                    excelConString = string.Format(excelConString, filePath);
+                    ViewBag.Message = "excelConString" + excelConString;
+                    using (OleDbConnection excelOledbConnection = new OleDbConnection(excelConString))
+                    {
+                        using (OleDbCommand excelDbCommand = new OleDbCommand())
+                        {
+                            using (OleDbDataAdapter excelDataAdapter = new OleDbDataAdapter())
+                            {
+                                excelDbCommand.Connection = excelOledbConnection;
+
+                                excelOledbConnection.Open();
+                                //Get schema from excel sheet
+                                DataTable excelSchema = GetSchemaFromExcel(excelOledbConnection);
+                                //Get sheet name
+                                string sheetName = excelSchema.Rows[0]["TABLE_NAME"].ToString();
+                                excelOledbConnection.Close();
+
+                                //Read Data from First Sheet.
+                                excelOledbConnection.Open();
+                                excelDbCommand.CommandText = "SELECT * From [" + sheetName + "]";
+                                excelDataAdapter.SelectCommand = excelDbCommand;
+                                //Fill datatable from adapter
+                                excelDataAdapter.Fill(dt);
+                                excelOledbConnection.Close();
+                            }
+                        }
+                    }
+
+                    using (var context = new Database1Entities())
+                    { 
+                        foreach (DataRow row in dt.Rows)
+                        {
+                            context.Subjects.Add(GetSubjectFromExcelRow(row));
+                        }
+
+                        context.SaveChanges();
+                    }
+                    ViewBag.Message = "Data Imported Successfully.";
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = "Catch: " + ex.Message;
+                }
+            }
+            else
+            {
+                ViewBag.Message = "Please select the file first to upload.";
+            }
+            return View();
+        }
+
+        private static DataTable GetSchemaFromExcel(OleDbConnection excelOledbConnection)
+        {
+            return excelOledbConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+        }
+
+        private Subject GetSubjectFromExcelRow(DataRow row)
+        {
+            return new Subject
+            {
+                SubID = row[0].ToString(),
+                SubName = row[1].ToString(),
+                CurrID = row[2].ToString(),
+                Sem = (int?)row[3],
+                Hours = (int?)row[4],
+            };
+        }
+
+        public ActionResult ExportToExcel()
+        {
+
+            DataTable dt = new DataTable("Subjects");
+            dt.Columns.AddRange(new DataColumn[5] { new DataColumn("SubID"),
+                                            new DataColumn("SubName"),
+                                            new DataColumn("CurrName"),
+                                            new DataColumn("Sem"),
+                                            new DataColumn("Hours")
+            });
+
+            var subject = from Subjects in db.Subjects.Take(10)
+                          select Subjects;
+
+            foreach (var st in subject)
+            {
+                dt.Rows.Add(st.SubID, st.SubName, st.Curriculum.CurrName, st.Sem, st.Hours);
+            }
+
+            using (XLWorkbook wb = new XLWorkbook())
+            {
+                wb.Worksheets.Add(dt);
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    wb.SaveAs(stream);
+                    return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Grid.xlsx");
+                }
+            }
+            return View("Index");
+        }
+
+        // GET: Subjects/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Subject subject = db.Subjects.Find(id);
+            if (subject == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subject);
+        }
+
+        // GET: Subjects/Create
+        public ActionResult Create()
+        {
+            ViewBag.CurrID = new SelectList(db.Curriculums, "CurrID", "CurrName");
+            return View();
+        }
+
+        // POST: Subjects/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SubID,SubName,CurrID,Sem,Hours")] Subject subject)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Subjects.Add(subject);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CurrID = new SelectList(db.Curriculums, "CurrID", "CurrName", subject.CurrID);
+            return View(subject);
+        }
+
+        // GET: Subjects/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Subject subject = db.Subjects.Find(id);
+            if (subject == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CurrID = new SelectList(db.Curriculums, "CurrID", "CurrName", subject.CurrID);
+            return View(subject);
+        }
+
+        // POST: Subjects/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SubID,SubName,CurrID,Sem,Hours")] Subject subject)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(subject).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CurrID = new SelectList(db.Curriculums, "CurrID", "CurrName", subject.CurrID);
+            return View(subject);
+        }
+
+        // GET: Subjects/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Subject subject = db.Subjects.Find(id);
+            if (subject == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subject);
+        }
+
+        // POST: Subjects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Subject subject = db.Subjects.Find(id);
+            db.Subjects.Remove(subject);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
